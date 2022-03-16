8167acb5aea1c4c9c355e3532030c9b5cef50373
AlexanderRod <alexander.rodrigo.calderon@gmail.com>
2022-03-16 15:36:42 -0500

    Labo01

---
+++ Laboratorio 01/01-ejercicio01.js
@@ -0,0 +1,47 @@
+/**
+ * Cree una función que reciba un número entero J y un arreglo de enteros, la función debe retornar el arreglo con los enteros desplazados hacia la izquierda según el entero J.
+ *
+ * Ej.
+ * J = 2
+ * arr = [1,2,3,4,5]
+ * RES: [3,4,5,1,2]
+ */
+const moverPosiciones = ( j, arreglo) => {
+    // Con función splice
+    // =========================================
+    console.log(arreglo);
+    let arrNuevo = arreglo.splice(0, j);
+    console.log(j);
+    let respuesta = arreglo.contact(arrNuevo);
+    return respuesta;
+
+    // Con funcion de for
+    // =========================================
+    // console.log(arreglo);
+    // let nuevoArreglo = [];
+    // for (let i = 0; i < arreglo.length; i++) {
+    //     if (i + j < arreglo.length) {
+    //         nuevoArreglo.push(arreglo[i + j]);
+    //     } else {
+    //         nuevoArreglo.push(arreglo[i + j - arreglo.length]);
+    //     }
+    // }
+    // return nuevoArreglo;
+};
+
+let j = +prompt("Ingrese el desplazamiento")
+/**
+ * Hacer que el usuario ingrese numeros y tener en un arreglo
+ */
+let continuar = true;
+let arrNum = [];
+do {
+    let ingreso = prompt("Ingrese un número o escriba x para dejar de ingresar números");
+    if (ingreso === "x") {
+        continuar = false;
+    } else {
+        arrNum.push(+ingreso);
+    }
+} while (continuar === true);
+console.log(moverPosiciones(j, arrNum))
+
